5c617cdb948932ce4b2e19eb85c1913c77ad70a9
part-time-indus <epic_indus_dev@gmx.com>
2024-02-03 21:54:59 +1100

    Chapter 3 Exercise 2. Not Tested!!

--- src/ch03/ex/Employee.java
+++ src/ch03/ex/Employee.java
@@ -2,8 +2,10 @@ package ch03.ex;
 
 public class Employee  implements  Measurable{
     double salary;
-    public Employee(double salary){
+    String name;
+    public Employee(String name, double salary){
         this.salary = salary;
+        this.name = name;
     }
     public int compareTo(Employee other){
         return Double.compare(salary, other.salary);
@@ -14,7 +16,7 @@ public class Employee  implements  Measurable{
         return this.salary;
     }
 
-    public double average(Measurable[] objects){
+    public static double average(Measurable[] objects){
         double sum = 0.0;
         for(Measurable obj: objects){
             sum = sum + obj.getMeasure();
@@ -22,4 +24,23 @@ public class Employee  implements  Measurable{
         }
         return sum/(objects.length);
     }
+
+    public static Measurable largest(Measurable[] objects){
+        Measurable largest = objects[0];
+        for(Measurable obj: objects){
+            if(obj.getMeasure() > largest.getMeasure()){
+                largest = obj;
+            }
+        }
+        return largest;
+    }
+
+    public static String highestSalary(Employee[] employees){
+        Employee result = (Employee) largest(employees);
+        if(result != null){
+            return result.name;
+        }
+        return "";
+    }
+
 }
