93c915898bbfeec7c7a895b965c17beb31cdd281
part-time-indus <epic_indus_dev@gmx.com>
2024-02-12 21:41:19 +1100

    Chapter 3 Exercise 11. Not Tested!!

--- src/ch03/ex/ListDirs.java
+++ src/ch03/ex/ListDirs.java
@@ -4,7 +4,14 @@ import java.io.File;
 import java.io.FileFilter;
 
 public class ListDirs {
-    public static File[] FetchSubDirs(String dirPath){
+
+    public static void main(String[] varargs){
+        File[] dirs = ListDirs.FetchSubDirsLambdaExp("D:\\documents");
+        for(File f:  dirs){
+            System.out.println(f.getName());
+        }
+    }
+    public static File[] FetchSubDirsMethodExp(String dirPath){
         File[] subdirs = null;
         File parentDir = new File(dirPath);
         FileFilter filter = File::isDirectory;
@@ -12,5 +19,27 @@ public class ListDirs {
             subdirs = parentDir.listFiles(filter);
         }
         return subdirs;
+    }
+
+    public static File[] FetchSubDirsLambdaExp(String dirPath){
+        File[] subDirs = null;
+        File parentDir = new File(dirPath);
+        if(parentDir.exists() && parentDir.isDirectory()) subDirs = parentDir.listFiles((f) -> f.isDirectory());
+        return subDirs;
+    }
+    public static File[] FetchSubDirsAnonCls(String dirPath){
+        File[] subDirs = null;
+        File parentDir = new File(dirPath);
+        if(parentDir.exists() && parentDir.isDirectory()) {
+            subDirs = parentDir.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.isDirectory();
+                }
+            });
+        }
+
+        return null;
+
     }
 }
