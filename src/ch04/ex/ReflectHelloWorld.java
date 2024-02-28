aff28337706b34d66a3ad75d211cb9f87c894e36
Vik Bhullar <epic_indus_dev@gmx.com>
2024-02-28 18:08:03 +1100

    Chapter 4 - Exercise 14

--- src/ch04/ex/ReflectHelloWorld.java
+++ src/ch04/ex/ReflectHelloWorld.java
@@ -12,12 +12,19 @@ public class ReflectHelloWorld {
         }
     }
     public static void print() throws NoSuchFieldException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        var start = System.nanoTime();
         Class<?> cl = java.lang.System.class;
-            var out = cl.getDeclaredField("out");
-            out.setAccessible(true);
-            var pStreamCls = out.getType();
-            var println = pStreamCls.getDeclaredMethod("println", String.class);
-            var underlyingObj = out.get(null);
-            println.invoke(underlyingObj,"Hello World");
+        var out = cl.getField("out");
+        var pStreamCls = out.getType();
+        var println = pStreamCls.getDeclaredMethod("println", String.class);
+        var underlyingObj = out.get(null);
+        println.invoke(underlyingObj,"Hello World");
+        var finish = System.nanoTime();
+        System.out.println("Time: "+ (finish-start) + "ns");
+        start = System.nanoTime();
+        System.out.println("Hello World");
+        finish = System.nanoTime();
+        System.out.println("Time: "+ (finish-start) + "ns");
+
     }
 }
