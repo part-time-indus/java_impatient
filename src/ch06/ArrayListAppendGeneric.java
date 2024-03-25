1e960fd21f795e4c580774e3eb12039e694e8014
part-time-indus <epic_indus_dev@gmx.com>
2024-03-25 22:11:22 +1100

    Chapter 6 Exercise 6. Untested!!!

---
+++ src/ch06/ArrayListAppendGeneric.java
@@ -0,0 +1,17 @@
+package ch06;
+import java.util.*;
+
+public class ArrayListAppendGeneric {
+
+    public static <S>void append(ArrayList<S> src, ArrayList<? super S> dest){
+        for(S e: src){
+            dest.add(e);
+        }
+    }
+
+    public static <S> void append(ArrayList<? extends S> src, ArrayList<S> dest){
+        for(S e: src){
+            dest.add(e);
+        }
+    }
+}
