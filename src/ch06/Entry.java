a438bc0f3b4fe8e93950e83c745f26ce2505f4a8
part-time-indus <epic_indus_dev@gmx.com>
2024-03-25 21:56:43 +1100

    Chapter 6 Exercise 5. Tested!!!

--- src/ch06/Entry.java
+++ src/ch06/Entry.java
@@ -16,6 +16,13 @@ public class Entry<K,V> {
         arr[j] = temp;
     }
 
+    public static <T> T[] swap2(int i, int j, T... values){
+        T temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+        return values;
+    }
+
     public void setValue(V value){
         this.value = value;
     }
@@ -23,6 +30,12 @@ public class Entry<K,V> {
     public K getKey() {return key;}
 
     public V getValue() {return value;}
+
+
+    public static void main(String[] args){
+//        Double[] result = Entry.swap2(0,1,1.5, 2,3);
+        Double[] result2 = Entry.<Double>swap2(0,1, 1.5,2.0,3.0);
+    }
 
 }
 
