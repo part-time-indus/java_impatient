1cb4ff9409f52d0dd6489bf2f8849f095718eeaa
part-time-indus <epic_indus_dev@gmx.com>
2024-03-26 20:20:28 +1100

    Chapter 6 Exercise 10. Untested!!!

--- src/ch06/Arrays.java
+++ src/ch06/Arrays.java
@@ -7,4 +7,25 @@ public class Arrays {
     public static <E extends Comparable<E>> Pair<E> firstLast(ArrayList<E> a){
         return new Pair<>(a.getFirst(), a.getLast());
     }
+
+    public static <E extends Comparable<E>> E min(ArrayList<E> a){
+        E min = a.getFirst();
+        for(int i = 1; i < a.size(); i++){
+            if(a.get(i).compareTo(min) < 0){
+                min = a.get(i);
+            }
+        }
+        return min;
+
+    }
+
+    public static <E extends Comparable<E>> E max(ArrayList<E> a){
+        E max = a.getFirst();
+        for(int i = 1; i < a.size(); i++){
+            if(a.get(i).compareTo(max) > 0){
+                max = a.get(i);
+            }
+        }
+        return max;
+    }
 }
